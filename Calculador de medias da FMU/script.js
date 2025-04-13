f4152571f0013076b908a097c03a3554048f0bda
Henrique de Castro Santos <henryttwoshoes@gmail.com>
2025-04-13 11:46:42 -0300

    adicionei mais funções ao site
    Agora o site consegue fazer o cálculo de média semestral e ponderada, além de conseguir trocar entre as duas. Emotes também aparecem quando o resultado sai no painel

--- Calculador de medias da FMU/script.js
+++ Calculador de medias da FMU/script.js
@@ -6,14 +6,26 @@ let situation = document.getElementById("situation")
 let placeholder = document.getElementById("array")
 let valores = [];
 let limite = 5;
-let contador = 0
+let contador = 0;
+let n1 = [];
+let n2 = 0;
+let tipo = document.getElementById('tipo')
+let calculosem = Boolean(true)
 function adicionar() {
     if (nota.value > 0 && nota.value <= 10 && contador < limite){
     var item = document.createElement('option')
-    item.text = `Valor ${nota.value} adicionado`
+    
     contador++
     valores.push(Number(nota.value))
     lista.appendChild(item)
+    if (contador <= 4){
+        item.text = `Valor ${nota.value} adicionado a nota N1`
+        n1.push(nota.value)
+    }
+    if (contador > 4 && contador == 5){
+        item.text = `Valor ${nota.value} adicionado a nota N2`
+        n2 = Number(nota.value)
+    }
     } 
     if (nota.value <= 0 || nota.value > 10) {
         window.alert('Valor inválido. Digite um valor entre 0 e 10.')
@@ -31,28 +43,65 @@ function limpar(){
     soma = 0
     media.innerText = ''
     situation.innerText = ''
+    img.outerHTML = ''
+}
+
+
+function trocatipo(){
+    let texttipo = document.getElementById('tipotext')
+    if (calculosem === true){
+        calculosem = false
+        texttipo.outerHTML = (`<p id="tipotext">Você está efetuando cálculo de <span style="color: rgb(236, 22, 22);">média simples</span></p>`)
+    }
+    else {
+        calculosem = true
+        texttipo.outerHTML = (`<p id="tipotext">Você está efetuando cálculo de <span style="color: rgb(22, 90, 236);">média semestral</span></p>`)
+    }
 }
 
 function calcular(){
-    let soma = 0;
-    let tot = valores.length;
-
+    if (valores != 0 && calculosem === true){
+    let mediasimples = 0;
+    let soma1 = 0;
+    //let tot1 = n1.length;
+    let img = document.getElementById('img')
     //Soma dos valores do array
-    for (let pos in valores){
-        soma += valores[pos];
+    for (let pos in n1){
+        soma1 += Number(n1[pos]);
     }
     
     //Calcula a média
-    
-    let mediaCalculada = soma / tot;
+    let median1 = soma1 / n1.length
+    let mediaCalculada = (median1*0.4) + (n2*0.6)
     
     //Exibe o resultado
     
-    media.innerText = `A média das notas expostas é ${mediaCalculada.toFixed(1)}`;
+    media.innerText = `A média das notas expostas é ${mediaCalculada}`;
     if (mediaCalculada < 6){
         situation.innerHTML = `<p id="situation" style="color: darkred;">Você está abaixo da média</p>`
+        img.outerHTML = `<img src="https://cdn.betterttv.net/emote/5fd1610acbd462462d56cd7d/3x.webp" id="img" alt="">`
     } else {
     situation.innerHTML = `<p id="situation" style="color: green;">Você está acima da média!</p>`
+    img.outerHTML = `<img src="https://cdn.frankerfacez.com/emoticon/318778/4" id="img" alt="">`
     }
-    
+} else if (calculosem === false){
+    let soma1 = 0
+    //let tot1 = n1.length;
+    let img = document.getElementById('img')
+    //Soma dos valores do array
+    for (let pos in valores){
+        soma1 += Number(valores[pos]);
+    }
+    let mediasimples = soma1 / valores.length
+    let mediaCalculada = mediasimples
+
+    media.innerText = `A média das notas expostas é ${mediaCalculada}`;
+    if (mediaCalculada < 6){
+        situation.innerHTML = `<p id="situation" style="color: darkred;">Você está abaixo da média</p>`
+        img.outerHTML = `<img src="https://cdn.betterttv.net/emote/5fd1610acbd462462d56cd7d/3x.webp" id="img" alt="">`
+    } else {
+    situation.innerHTML = `<p id="situation" style="color: green;">Você está acima da média!</p>`
+    img.outerHTML = `<img src="https://cdn.frankerfacez.com/emoticon/318778/4" id="img" alt="">`
+    }
+}
 }
